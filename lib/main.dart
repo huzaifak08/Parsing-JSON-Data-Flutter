48806f2b6697d45938db25250c5745a7b6bba66e
huzaifak08 <hk7928042@gmail.com>
2022-08-05 16:44:56 +0500

    Another JSON Parsing using Assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:playing_with_json/another.dart';
 import 'package:playing_with_json/home.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Home(),
+      home: const Another(),
     );
   }
 }
